51b5ec32aaafd7a3f7789d4af4b4e88dac01b16d
Erik Berggren <eribe@lap5cg9151b31.se.axis.com>
2019-08-30 14:55:15 +0200

    last update

--- public/js/game.js
+++ public/js/game.js
@@ -9,7 +9,7 @@ var config = {
                 x: 0,
                 y: 0
             },
-            debug: true
+            debug: false
         }
     },
     scene: {
@@ -24,6 +24,9 @@ var cursors;
 var platforms;
 var player;
 var cameraAreas;
+var cameras;
+var worker;
+var thief;
 
 function preload ()
 {
@@ -38,7 +41,10 @@ function preload ()
     this.load.image('triangle-left', '../assets/triangle-left.png');
     this.load.image('triangle-right', '../assets/triangle-right.png');
     this.load.image('stairs', '../assets/stairs.png');
-
+    this.load.image('cleft', '../assets/camera-right.png');
+    this.load.image('cright', '../assets/camera-left.png');
+    this.load.image('worker', '../assets/axis-worker.png');
+    this.load.image('thief', '../assets/thief.png');
 
 }
 
@@ -58,14 +64,25 @@ function create ()
 
     platforms = this.physics.add.staticGroup();
     cameraAreas = this.physics.add.staticGroup();
-
+    cameras = this.physics.add.staticGroup();
+    worker = this.physics.add.staticGroup();
+    thief = this.physics.add.staticGroup();
 
     platforms.create(600, 500, 'table').setScale(0.4).refreshBody();
     platforms.create(120, 200, 'plant').setScale(0.3).refreshBody();
     platforms.create(400, 150, 'cat').setScale(0.09).refreshBody();
     platforms.create(250, 50, 'stairs').setScale(0.3).refreshBody();
-    cameraAreas.create(150, 400, 'triangle-left').setScale(0.8).refreshBody();
-    cameraAreas.create(650, 160, 'triangle-right').setScale(0.8).refreshBody();
+    cameraAreas.create(200, 400, 'triangle-left').setScale(0.8).refreshBody();
+    cameraAreas.create(600, 160, 'triangle-right').setScale(0.8).refreshBody();
+    
+    
+    
+    cameras.create(40,410, 'cright').setScale(0.25).refreshBody();
+    cameras.create(750,150, 'cleft').setScale(0.25).refreshBody();
+
+    worker.create(50, 500, 'worker').setScale(0.25).refreshBody();
+
+    thief.create(400,50, 'thief').setScale(0.25).refreshBody();
 
 
     this.physics.add.collider(player, platforms);
@@ -79,7 +96,11 @@ function create ()
         }
     );
 
-    this.physics.add.overlap(player, cameraAreas);
+    this.physics.add.overlap(player, cameraAreas, function() {
+        console.log ('YOU LOSE!');
+
+        gameOver = true;
+    });
 
 }
 
